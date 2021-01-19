2e31476fdfff66064857b581e3722aff0e42dd9d
Kidane Yitbarek <josiah103@yahoo.com>
2021-01-19 02:37:00 -0800

    comminting the right code

--- connect.js
+++ connect.js
@@ -1,187 +1,165 @@
-var gameField = new Array();
-var board = document.getElementById("game-table");
-var currentCol;
-var currentRow;
-var currentPlayer;
-var id = 1;
+/** Connect Four
+ *
+ * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
+ * column until a player gets four-in-a-row (horiz, vert, or diag) or until
+ * board fills (tie)
+ */
+const tableRow = document.getElementsByTagName('tr');
+const tableCell = document.getElementsByTagName('td');
+const tableSlot = document.querySelectorAll('.slot');
+const playerTurn = document.querySelector(".Player-Turn");
+const reset = document.querySelector('.reset');
 
-newgame();
-
-function newgame(){
-  prepareField();
-  placeDisc(Math.floor(Math.random()*2)+1);
+for(let i = 0; i < tableCell.length; i++){
+  tableCell[i].addEventListener('click', (e) =>{
+    console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`);
+  })
 }
 
-function checkForVictory(row,col){
-  if(getAdj(row,col,0,1)+getAdj(row,col,0,-1) > 2){
-    return true;
-  } else {
-    if(getAdj(row,col,1,0) > 2){
-      return true;
-    } else {
-      if(getAdj(row,col,-1,1)+getAdj(row,col,1,-1) > 2){
-        return true;
-      } else {
-        if(getAdj(row,col,1,1)+getAdj(row,col,-1,-1) > 2){
+while(!player1){
+  var player1 = prompt('Player One: Enter your name. You will be red. ')
+}
+
+player1Color = 'red';
+
+while(!player2){
+  var player2 = prompt('Player Two: Enter your name. You will be yellow. ')
+}
+
+player2Color = 'yellow';
+
+var currentPlayer = 1;
+playerTurn.textContent = `${player1}'s turn!`;
+
+
+
+Array.prototype.forEach.call(tableCell, (cell) =>{
+  cell.addEventListener('click', changeColor);
+  cell.style.backgroundColor = 'white';
+});
+
+function changeColor(e){
+  let column = e.target.cellIndex;
+  let row = [];
+
+  for (let i = 5; i > -1; i--){
+    if(tableRow[i].children[column].style.backgroundColor == 'white'){
+    row.push(tableRow[i].children[column]);
+    if(currentPlayer === 1){
+     row[0].style.backgroundColor = player1Color;
+     if(horizontalCheck() || verticalCheck() || diagonalCheck1() || diagonalCheck2() ){
+       playerTurn.textContent = `${player1} wins!`;
+       playerTurn.style.color = player1Color;
+      return(alert(`${player1} WINS!!`))
+     }else if(drawCheck()){
+       playerTurn.textContent = 'Game is a draw!';
+       return alert('DRAW');
+     }else{
+       playerTurn.textContent = `${player2}'s turn!`;
+       return currentPlayer = 2;
+     }
+     
+     
+    }else{
+      row[0].style.backgroundColor = player2Color;
+      playerTurn.textContent = `${player1}'s turn!`;
+      if(horizontalCheck() || verticalCheck() || diagonalCheck1() || diagonalCheck2() ){
+        playerTurn.textContent = `${player2} wins!`;
+        playerTurn.style.color = player2Color;
+       return(alert(`${player2} WINS!!`))
+      }else if(drawCheck()){
+        playerTurn.textContent = "Game is a draw!";
+        return alert('DRAW');
+      }else{
+        playerTurn.textContent = `${player1}'s turn!`;
+        return currentPlayer = 1;
+      }
+      }
+    }
+    }
+  }
+
+
+function colorMatchCheck(one,two,three,four){
+  return(one == two && one === three && one === four & one !== 'white' );
+}
+
+function horizontalCheck(){
+  for(let row = 0; row < tableRow.length; row++){
+    for(let col = 0; col < 4; col++){
+      if(colorMatchCheck(
+        tableRow[row].children[col].style.backgroundColor, 
+        tableRow[row].children[col+1].style.backgroundColor, 
+        tableRow[row].children[col+2].style.backgroundColor, 
+        tableRow[row].children[col+3].style.backgroundColor,
+        )){
           return true;
-        } else {
-          return false;
         }
-      }
+    }
+  }
+};
+
+function verticalCheck(){
+  for(let col = 0; col < 7; col++){
+    for(let row = 0; row < 3; row++){
+      if(colorMatchCheck(
+        tableRow[row].children[col].style.backgroundColor,
+        tableRow[row+1].children[col].style.backgroundColor,
+        tableRow[row+2].children[col].style.backgroundColor,
+        tableRow[row+3].children[col].style.backgroundColor,
+        )){
+          return true;
+        }
     }
   }
 }
 
-function getAdj(row,col,row_inc,col_inc){
-  if(cellVal(row,col) == cellVal(row+row_inc,col+col_inc)){
-    return 1+getAdj(row+row_inc,col+col_inc,row_inc,col_inc);
-  } else {
-    return 0;
-  }
-}
-
-function cellVal(row,col){
-  if(gameField[row] == undefined || gameField[row][col] == undefined){
-    return -1;
-  } else {
-    return gameField[row][col];
-  }
-}
-
-function firstFreeRow(col,player){
-  for(var i = 0; i<6; i++){
-    if(gameField[i][col]!=0){
-      break;
-    }
-  }
-  gameField[i-1][col] = player;
-  return i-1;
-}
-
-function possibleColumns(){
-  var moves_array = new Array();
-  for(var i=0; i<7; i++){
-    if(gameField[0][i] == 0){
-      moves_array.push(i);
-    }
-  }
-  return moves_array;
-}
-
-function think(){
-  var possibleMoves = possibleColumns();
-  var aiMoves = new Array();
-  var blocked;
-  var bestBlocked = 0;
-  
-  for(var i=0; i<possibleMoves.length; i++){
-    for(var j=0; j<6; j++){
-      if(gameField[j][possibleMoves[i]] != 0){
-        break;
-      }
-    }
-    
-    gameField[j-1][possibleMoves[i]] = 1;
-    blocked = getAdj(j-1,possibleMoves[i],0,1)+getAdj(j-1,possibleMoves[i],0,-1);
-    blocked = Math.max(blocked,getAdj(j-1,possibleMoves[i],1,0));
-    blocked = Math.max(blocked,getAdj(j-1,possibleMoves[i],-1,1));
-    blocked = Math.max(blocked,getAdj(j-1,possibleMoves[i],1,1)+getAdj(j-1, possibleMoves[i],-1,-1));
-    
-    if(blocked >= bestBlocked){
-      if(blocked>bestBlocked){
-        bestBlocked = blocked;
-        aiMoves = new Array();
-      }
-      aiMoves.push(possibleMoves[i]);
-    }
-    gameField[j-1][possibleMoves[i]] = 0;
-  }
-  
-  return aiMoves;
-}
-
-function Disc(player){
-  this.player = player;
-  this.color = player == 1 ? 'red' : 'yellow';
-  this.id = id.toString();
-  id++;
-  
-  this.addToScene = function(){
-    board.innerHTML += '<div id="d'+this.id+'" class="disc '+this.color+'"></div>';
-    if(currentPlayer==2){
-      //computer move
-      var possibleMoves = think();
-      var cpuMove = Math.floor( Math.random() * possibleMoves.length);
-      currentCol = possibleMoves[cpuMove];
-      document.getElementById('d'+this.id).style.left = (14+60*currentCol)+"px";
-      dropDisc(this.id,currentPlayer);
-    }
-  }
-  
-  var $this = this;
-  document.onmousemove = function(evt){
-    if(currentPlayer == 1){
-    currentCol = Math.floor((evt.clientX - board.offsetLeft)/60);
-    if(currentCol<0){currentCol=0;}
-    if(currentCol>6){currentCol=6;}
-    document.getElementById('d'+$this.id).style.left = (14+60*currentCol)+"px";
-    document.getElementById('d'+$this.id).style.top = "-55px";
-    }
-  }
-  document.onload = function(evt){
-    if(currentPlayer == 1){
-    currentCol = Math.floor((evt.clientX - board.offsetLeft)/60);
-    if(currentCol<0){currentCol=0;}
-    if(currentCol>6){currentCol=6;}
-    document.getElementById('d'+$this.id).style.left = (14+60*currentCol)+"px";
-    document.getElementById('d'+$this.id).style.top = "-55px";
-    }
-  }
-  
-  document.onclick = function(evt){
-    if(currentPlayer == 1){
-      if(possibleColumns().indexOf(currentCol) != -1){
-        dropDisc($this.id,$this.player);
-      }
+function diagonalCheck1(){
+  for(let col = 0; col < 4; col++){
+    for(row = 0; row <3; row++){
+      if(colorMatchCheck(
+          tableRow[row].children[col].style.backgroundColor,
+          tableRow[row+1].children[col+1].style.backgroundColor,
+          tableRow[row+2].children[col+2].style.backgroundColor,
+          tableRow[row+3].children[col+3].style.backgroundColor,
+        )){
+          return true;
+        }
     }
   }
 }
 
-function dropDisc(cid,player){
-  currentRow = firstFreeRow(currentCol,player);
-  moveit(cid,(14+currentRow*60));
-  currentPlayer = player;
-  checkForMoveVictory();
-}
-
-function checkForMoveVictory(){
-  if(!checkForVictory(currentRow,currentCol)){
-    placeDisc(3-currentPlayer);
-  } else {
-    var ww = currentPlayer == 2 ? 'Computer' : 'Player';
-    placeDisc(3-currentPlayer);
-    alert(ww+" win!");
-    board.innerHTML = "";
-    newgame();
-  }
-}
-
-function placeDisc(player){
-  currentPlayer = player;
-  var disc = new Disc(player);
-  disc.addToScene();
-}
-
-function prepareField(){
-  gameField = new Array();
-  for(var i=0; i<6; i++){
-    gameField[i] = new Array();
-    for(var j=0; j<7; j++){
-      gameField[i].push(0);
+function diagonalCheck2(){
+  for(let col = 0; col < 4; col++){
+    for(row = 5; row > 2; row--){
+      if(colorMatchCheck(
+          tableRow[row].children[col].style.backgroundColor,
+          tableRow[row-1].children[col+1].style.backgroundColor,
+          tableRow[row-2].children[col+2].style.backgroundColor,
+          tableRow[row-3].children[col+3].style.backgroundColor,
+        )){
+          return true;
+        }
     }
   }
 }
 
-function moveit(who,where){
-    document.getElementById('d'+who).style.top = where+'px';
-}
+function drawCheck(){
+  let fullSlot = [];
+  for(let i = 0; i < tableCell.length; i++){
+    if(tableCell[i].style.backgroundColor !== 'white'){
+      fullSlot.push(tableCell[i]);
+    }
+  }
+  if(fullSlot.length === tableCell.length){
+    return true;
+  }
+}reset.addEventListener('click', ()=>{
+  tableSlot.forEach(slot =>{
+    slot.style.backgroundColor = 'white';
+
+  });
+  playerTurn.style.color = 'black';
+  return(currentPlayer === 1 ? playerTurn.textContent = `${player1}'s turn`: playerTurn.textContent = `${player2}'s turn`);
+})
+
